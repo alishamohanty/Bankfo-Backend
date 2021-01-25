fbfc3e4782b2ff475142f2b1d1cad45c29c0f5a3
Alisha Mohanty <alishamohanty1416@gmail.com>
2021-01-26 00:44:38 +0530

    Initialize sequilize to connect to PostgresSQL

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@ const path = require('path')
 module.exports = {
     port: process.env.PORT || 8082,
     db: {
-        databaseName: process.env.DB_NAME || 'demo',
+        database: process.env.DB_NAME || 'database',
         user: process.env.USER || 'user',
         password: process.env.PASSWORD || 'password',
         options: {
-            host: process.env.HOST || 'host',
-            connectionURI: process.env.connectionURI || ''
+            host: process.env.HOST || 'localhost',
+            dialect: 'postgres'
         }
     }
 }
